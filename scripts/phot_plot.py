0e13a13b553c519f89d0316f117f8c8eddafc3fb
Ryan Oelkers <ryanjamesoelkers@gmail.com>
2025-06-16 17:52:31 -0500

    updates to de-trending photometry

--- scripts/phot_plot.py
+++ scripts/phot_plot.py
@@ -3,17 +3,114 @@ import matplotlib
 matplotlib.use('TkAgg')
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from scipy.signal import medfilt
 
-lc2 = pd.read_csv("/Users/yuw816/Development/etsi_solarsystem/analysis/band2.txt",
-                  names=['jd', 'flx', 'bkg', 'x', 'y', 'ams'], sep=" ")
-lc2['mag'] = lc2.apply(lambda x: 25 - 2.5 * np.log10(x.flx - x.bkg), axis=1)
-lc5 = pd.read_csv("/Users/yuw816/Development/etsi_solarsystem/analysis/band5.txt",
-                  names=['jd', 'flx', 'bkg', 'x', 'y', 'ams'], sep=" ")
-lc5['mag'] = lc5.apply(lambda x: 25 - 2.5 * np.log10(x.flx - x.bkg), axis=1)
+flx = ["ap1", "ap2", "ap3", "ap4", "ap5", "ap6", "ap7", "ap8"]
+mag = ["m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8"]
+rmg = ["mr1", "mr2", "mr3", "mr4", "mr5", "mr6", "mr7"]
+bkg = ["bkg1", "bkg2", "bkg3", "bkg4", "bkg5", "bkg6", "bkg7", "bkg8"]
+x = ["x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8"]
+y = ["y1", "y2", "y3", "y4", "y5", "y6", "y7", "y8"]
+xr = ["xr1", "xr2", "xr3", "xr4", "xr5", "xr6", "xr7"]
+yr = ["yr1", "yr2", "yr3", "yr4", "yr5", "yr6", "yr7"]
 
-# plt.plot(lc2.jd, lc2.mag - lc2.mag.min(), c='r')
-# plt.plot(lc5.jd, lc5.mag - lc5.mag.min(), c='b')
-# plt.show()
-# np.sqrt((lc2.x - lc5.x) ** 2 + (lc2.y - lc5.y) ** 2)
-plt.plot(lc5.ams, lc5.mag - lc2.mag, c='k')
+tr = pd.read_csv("C:\\Users\\ryanj\\Development\\etsi_solarsystem\\analysis\\titan_transmission.txt",
+                 sep=" ")
+rf = pd.read_csv("C:\\Users\\ryanj\\Development\\etsi_solarsystem\\analysis\\titan_reflection.txt",
+                 sep=" ")
+
+tr = tr[:len(rf)].reset_index(drop=True)
+for idx, f in enumerate(flx):
+    tr[mag[idx]] = tr.apply(lambda x: 25 - 2.5 * np.log10(x[f] - x[bkg[idx]]), axis=1)
+    if idx < 7:
+        tr[rmg[idx]] = rf.apply(lambda x: 25 - 2.5 * np.log10(x[f] - x[bkg[idx]]), axis=1)
+        tr[xr[idx]] = rf[x[idx]].to_numpy()
+        tr[yr[idx]] = rf[y[idx]].to_numpy()
+
+bin_size = 0.000060
+bin_strt = tr.jd.min()
+bin_end = tr.jd.max()
+tr['time_bin'] = 0
+ii = 0
+
+while bin_strt < bin_end:
+    tr.loc[(tr.jd > bin_strt) & (tr.jd <= bin_strt + bin_size), 'time_bin'] = ii
+    bin_strt = bin_strt + bin_size
+    ii = ii + 1
+
+df = tr.groupby('time_bin').agg({'jd': 'mean', 'airmass': 'mean',
+                                 'x1':'mean', 'y1': 'mean','x2':'mean', 'y2': 'mean',
+                                 'x3':'mean', 'y3': 'mean','x4':'mean', 'y4': 'mean',
+                                 'x5':'mean', 'y5': 'mean','x6':'mean', 'y6': 'mean',
+                                 'x7':'mean', 'y7': 'mean','x8':'mean', 'y8': 'mean',
+                                 'm1': 'median', 'm2':'median',
+                                 'm3': 'median', 'm4':'median',
+                                 'm5': 'median', 'm6':'median',
+                                 'm7': 'median', 'm8':'median',
+                                 'mr1': 'median', 'mr2':'median',
+                                 'mr3': 'median', 'mr4':'median',
+                                 'mr5': 'median', 'mr6':'median',
+                                 'xr1': 'mean', 'yr1': 'mean', 'xr2': 'mean', 'yr2': 'mean',
+                                 'xr3': 'mean', 'yr3': 'mean', 'xr4': 'mean', 'yr4': 'mean',
+                                 'xr5': 'mean', 'yr5': 'mean', 'xr6': 'mean', 'yr6': 'mean',
+                                 'xr7': 'mean', 'yr7': 'mean',
+                                 'mr7': 'median'})
+
+df = df[(df.jd <= 2459769.9) & (df.jd >= 2459769.875)].copy().reset_index(drop=True)
+oot = df[(df.jd <= 2459769.884) | (df.jd >= 2459769.894)].copy().reset_index(drop=True)
+
+cc = ['maroon', 'red', 'orange', 'brown', 'yellow', 'gold', 'green',
+      'darkgreen', 'cyan', 'dodgerblue', 'blue', 'purple', 'violet', 'pink', 'black']
+deps = np.zeros(15)
+wvs = np.array([937, 873, 763, 713, 660, 620, 587, 559, 533, 512, 494, 476, 467, 448, 435])
+
+from sklearn.linear_model import LinearRegression
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import ConstantKernel, RBF
+xlist = ['jd']
+pp = oot[xlist]
+depths = np.zeros(15)
+for idx, m in enumerate(mag):
+
+    yy = oot[m] - oot[mag[1]]
+
+    rr = df[xlist]
+    vv = LinearRegression().fit(pp,yy).predict(rr)
+
+    kernel = ConstantKernel() * RBF()
+    # GP model
+    gp = GaussianProcessRegressor()
+    # Fitting in the gp model
+    gp.fit(pp, yy)
+    # Make the prediction on test set.
+    v = gp.predict(rr)
+
+    if idx == 1:
+        depths[idx * 2] = 0
+    else:
+        df['cln'] = (df[m] - df[mag[1]]) - v
+
+        depths[idx * 2] = df[(df.jd < 2459769.89) & (df.jd > 2459769.888)].cln.mean()
+
+    if idx < 7:
+
+        yy = oot[rmg[idx]] - oot[mag[1]]
+
+        rr = df[xlist]
+        vv = LinearRegression().fit(pp, yy).predict(rr)
+
+        kernel = ConstantKernel() * RBF()
+        # GP model
+        gp = GaussianProcessRegressor()
+        # Fitting in the gp model
+        gp.fit(pp, yy)
+        # Make the prediction on test set.
+        v = gp.predict(rr)
+        df['cln'] = (df[rmg[idx]] - df[mag[1]]) - v
+        depths[idx * 2 + 1] = df[(df.jd < 2459769.89) & (df.jd > 2459769.888)].cln.mean()
+
+plt.plot(wvs, depths)
+#plt.gca().invert_yaxis()
 plt.show()
+print('hold')
--- scripts/phot_testing.py
+++ scripts/phot_testing.py
@@ -1,102 +1,134 @@
-import photutils
-import pandas as pd
-import astropy
-from astropy.io import fits
 from libraries.utils import Utils
-
-dir = "/Users/yuw816/Data/solarsystem/titan/images/coadded/"
-
-files = Utils.get_file_list(dir, ".fits")
-
+from skimage import io
+from astropy.time import Time
 from photutils.aperture import RectangularAperture, aperture_photometry, ApertureStats
 from photutils.centroids import centroid_com
 import numpy as np
-x=975
-y=886
+from astropy.io import fits
+from libraries.utils import Utils
+
+dir = "G:\\July2022\\2022-07-08\\Titan\\raw\\transmission\\"
+
+files = Utils.get_file_list(dir, ".fits")
+
 x_box = 84
 y_box = 26
 
-flx = np.zeros(len(files))
-tme = np.zeros(len(files))
-bkg = np.zeros(len(files))
-xs = np.zeros(len(files))
-ys = np.zeros(len(files))
-exp = np.zeros(len(files))
+xx = (886.5012, 975.59263, 1058.5634, 1144.091, 1239.5534, 1331.9513, 1435.4845, 1537.0783)
+yy = (885.65929, 886.68251, 886.21227, 885.52201, 887.48408, 888.54775, 889.59711, 888.6933)
 
-f = open("/Users/yuw816/Development/etsi_solarsystem/analysis/band2.txt", "w+")
+
+f = open("C:\\Users\\barristan\\Development\\etsi_development\\analysis\\titan_transmission.txt", "w+")
 for idx, file in enumerate(files):
-
     data = fits.getdata(dir + file)
-    data_c = data[int(y-y_box/2):int(y+y_box/2), int(x-x_box/2):int(x+x_box/2)]
     hdr = fits.getheader(dir + file)
 
-    x1, y1 = centroid_com(data_c)
+    positions = []
+    sky_pos = []
 
-    positions = [(np.around(x-x_box/2 + x1, decimals=2), np.around(y-y_box/2 + y1, decimals=2))]
+    for x, y in zip(xx, yy):
 
-    x = np.around(x-x_box/2 + x1, decimals=2)
-    y = np.around(y-y_box/2 + y1, decimals=2)
+        data_c = data[int(y-y_box/2):int(y+y_box/2), int(x-x_box/2):int(x+x_box/2)]
+
+        x1, y1 = centroid_com(data_c)
+
+        positions.append((np.around(x-x_box/2 + x1, decimals=2), np.around(y-y_box/2 + y1, decimals=2)))
+        sky_pos.append((np.around(x-x_box/2 + x1, decimals=2), np.around(y-y_box/2 + y1 + 100, decimals=2)))
 
     aperture = RectangularAperture(positions, w=x_box, h=y_box)
-    phot_table = aperture_photometry(data, aperture)
+    sky = RectangularAperture(sky_pos, w=x_box, h=y_box)
+
     apt_stat = ApertureStats(data, aperture)
-    flx[idx] = phot_table['aperture_sum'].item() / hdr['EXP_TIME']
-    tme[idx] = np.mean([hdr['COADD_ST'], hdr['COADD_ED']])
-    xs[idx] = x
-    ys[idx] = y
-    exp[idx] = np.mean([hdr['AIRMASSS'], hdr['AIRMASSE']])
-    positions = [(np.around(x-x_box/2 + x1 + 100, decimals=2), np.around(y-y_box/2 + y1 + 100, decimals=2))]
+    sky_stat = ApertureStats(data, sky)
 
-    aperture = RectangularAperture(positions, w=x_box, h=y_box)
-    phot_table = aperture_photometry(data, aperture)
-    bkg[idx] = phot_table['aperture_sum'].item() / hdr['EXP_TIME']
+    if idx == 0:
+        f.write("jd exp_time airmass ha zd ra dec humidity temp "
+                "ap1 bkg1 x1 y1 ap2 bkg2 x2 y2 ap3 bkg3 x3 y3 ap4 bkg4 x4 y4 "
+                "ap5 bkg5 x5 y5 ap6 bkg6 x6 y6 ap7 bkg7 x7 y7 ap8 bkg8 x8 y8\n")
 
-    f.write(str(np.around(tme[idx], decimals=6)) + " " + str(np.around(flx[idx], decimals=2)) + " " +
-            str(np.around(bkg[idx], decimals=2)) + " " + str(np.around(xs[idx], decimals=2)) + " " +
-            str(np.around(ys[idx], decimals=2)) + " " + str(np.around(exp[idx], decimals=4)) + "\n")
+    f.write(str(np.around(Time(hdr['DATE-OBS']).jd, decimals=6)) + " " +
+            str(np.around(hdr['HIERARCH EXPOSURE TIME'], decimals=6)) + " " +
+            str(np.around(hdr['AIRMASS'], decimals=6)) + " " +
+            str(np.around(hdr['HIERARCH OBSERVED_HA_HOUR'], decimals=6)) + " " +
+            str(np.around(hdr['HIERARCH OBSERVED_ZD_DEGREE'], decimals=6)) + " " +
+            str(np.around(hdr['HIERARCH FLEX_RA_HOUR'], decimals=6)) + " " +
+            str(np.around(hdr['HIERARCH FLEX_DEC_DEGREE'], decimals=6)) + " " +
+            str(np.around(hdr['HIERARCH HUMIDITY_PERCENT'], decimals=6)) + " " +
+            str(np.around(hdr['HIERARCH TEMPERATURE_C'], decimals=6)) + " " +
+            str(np.around(apt_stat.sum[0], decimals=0)) + " " + str(np.around(sky_stat.sum[0], decimals=0)) + " " +
+            str(np.around(positions[0][0], decimals=2)) + " " + str(np.around(positions[0][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[1], decimals=0)) + " " + str(np.around(sky_stat.sum[1], decimals=0)) + " " +
+            str(np.around(positions[1][0], decimals=2)) + " " + str(np.around(positions[1][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[2], decimals=0)) + " " + str(np.around(sky_stat.sum[2], decimals=0)) + " " +
+            str(np.around(positions[2][0], decimals=2)) + " " + str(np.around(positions[2][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[3], decimals=0)) + " " + str(np.around(sky_stat.sum[3], decimals=0)) + " " +
+            str(np.around(positions[3][0], decimals=2)) + " " + str(np.around(positions[3][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[4], decimals=0)) + " " + str(np.around(sky_stat.sum[4], decimals=0)) + " " +
+            str(np.around(positions[4][0], decimals=2)) + " " + str(np.around(positions[4][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[5], decimals=0)) + " " + str(np.around(sky_stat.sum[5], decimals=0)) + " " +
+            str(np.around(positions[5][0], decimals=2)) + " " + str(np.around(positions[5][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[6], decimals=0)) + " " + str(np.around(sky_stat.sum[6], decimals=0)) + " " +
+            str(np.around(positions[6][0], decimals=2)) + " " + str(np.around(positions[6][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[7], decimals=0)) + " " + str(np.around(sky_stat.sum[7], decimals=0)) + " " +
+            str(np.around(positions[7][0], decimals=2)) + " " + str(np.around(positions[7][1], decimals=2)) + "\n")
+
+    if idx % 1000 == 0:
+        Utils.log("Image completed. " + str(len(files)-(idx + 1)) + " files remain.", "info")
 f.close()
 
-x=1240
-y=887
-x_box = 84
-y_box = 26
+dir = "G:\\July2022\\2022-07-08\\Titan\\raw\\reflection\\"
 
-flx = np.zeros(len(files))
-tme = np.zeros(len(files))
-bkg = np.zeros(len(files))
-xs = np.zeros(len(files))
-ys = np.zeros(len(files))
-exp = np.zeros(len(files))
+files = Utils.get_file_list(dir, ".tiff")
 
-f = open("/Users/yuw816/Development/etsi_solarsystem/analysis/band5.txt", "w+")
+x_box = 140
+y_box = 60
+
+xx = (867.14183, 1024.1916, 1193.6613, 1336.828, 1501.4269, 1664.5681, 1825.3117)
+yy = (1354.5117, 1354.2431, 1353.4492, 1352.5239, 1343.0083, 1339.4632, 1341.8843)
+
+f = open("C:\\Users\\barristan\\Development\\etsi_development\\analysis\\titan_reflection.txt", "w+")
+
 for idx, file in enumerate(files):
 
-    data = fits.getdata(dir + file)
-    data_c = data[int(y-y_box/2):int(y+y_box/2), int(x-x_box/2):int(x+x_box/2)]
-    hdr = fits.getheader(dir + file)
+    data = io.imread(dir + file)
 
-    x1, y1 = centroid_com(data_c)
+    positions = []
+    sky_pos = []
 
-    positions = [(np.around(x-x_box/2 + x1, decimals=2), np.around(y-y_box/2 + y1, decimals=2))]
+    for x, y in zip(xx, yy):
 
-    x = np.around(x-x_box/2 + x1, decimals=2)
-    y = np.around(y-y_box/2 + y1, decimals=2)
+        data_c = data[int(y-y_box/2):int(y+y_box/2), int(x-x_box/2):int(x+x_box/2)]
+
+        x1, y1 = centroid_com(data_c)
+
+        positions.append((np.around(x-x_box/2 + x1, decimals=2), np.around(y-y_box/2 + y1, decimals=2)))
+        sky_pos.append((np.around(x-x_box/2 + x1, decimals=2), np.around(y-y_box/2 + y1 + 100, decimals=2)))
 
     aperture = RectangularAperture(positions, w=x_box, h=y_box)
-    phot_table = aperture_photometry(data, aperture)
+    sky = RectangularAperture(sky_pos, w=x_box, h=y_box)
+
     apt_stat = ApertureStats(data, aperture)
-    flx[idx] = phot_table['aperture_sum'].item() / hdr['EXP_TIME']
-    tme[idx] = np.mean([hdr['COADD_ST'], hdr['COADD_ED']])
-    xs[idx] = x
-    ys[idx] = y
-    exp[idx] = np.mean([hdr['AIRMASSS'], hdr['AIRMASSE']])
-    positions = [(np.around(x-x_box/2 + x1 + 100, decimals=2), np.around(y-y_box/2 + y1 + 100, decimals=2))]
+    sky_stat = ApertureStats(data, sky)
 
-    aperture = RectangularAperture(positions, w=x_box, h=y_box)
-    phot_table = aperture_photometry(data, aperture)
-    bkg[idx] = phot_table['aperture_sum'].item() / hdr['EXP_TIME']
+    if idx == 0:
+        f.write("ap1 bkg1 x1 y1 ap2 bkg2 x2 y2 ap3 bkg3 x3 y3 ap4 bkg4 x4 y4 "
+                "ap5 bkg5 x5 y5 ap6 bkg6 x6 y6 ap7 bkg7 x7 y7\n")
 
-    f.write(str(np.around(tme[idx], decimals=6)) + " " + str(np.around(flx[idx], decimals=2)) + " " +
-            str(np.around(bkg[idx], decimals=2)) + " " + str(np.around(xs[idx], decimals=2)) + " " +
-            str(np.around(ys[idx], decimals=2)) + " " + str(np.around(exp[idx], decimals=4)) + "\n")
+    f.write(str(np.around(apt_stat.sum[0], decimals=0)) + " " + str(np.around(sky_stat.sum[0], decimals=0)) + " " +
+            str(np.around(positions[0][0], decimals=2)) + " " + str(np.around(positions[0][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[1], decimals=0)) + " " + str(np.around(sky_stat.sum[1], decimals=0)) + " " +
+            str(np.around(positions[1][0], decimals=2)) + " " + str(np.around(positions[1][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[2], decimals=0)) + " " + str(np.around(sky_stat.sum[2], decimals=0)) + " " +
+            str(np.around(positions[2][0], decimals=2)) + " " + str(np.around(positions[2][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[3], decimals=0)) + " " + str(np.around(sky_stat.sum[3], decimals=0)) + " " +
+            str(np.around(positions[3][0], decimals=2)) + " " + str(np.around(positions[3][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[4], decimals=0)) + " " + str(np.around(sky_stat.sum[4], decimals=0)) + " " +
+            str(np.around(positions[4][0], decimals=2)) + " " + str(np.around(positions[4][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[5], decimals=0)) + " " + str(np.around(sky_stat.sum[5], decimals=0)) + " " +
+            str(np.around(positions[5][0], decimals=2)) + " " + str(np.around(positions[5][1], decimals=2)) + " " +
+            str(np.around(apt_stat.sum[6], decimals=0)) + " " + str(np.around(sky_stat.sum[6], decimals=0)) + " " +
+            str(np.around(positions[6][0], decimals=2)) + " " + str(np.around(positions[6][1], decimals=2)) + "\n")
+
+    if idx % 1000 == 0:
+        Utils.log("Image completed. " + str(len(files)-(idx + 1)) + " files remain.", "info")
 f.close()
